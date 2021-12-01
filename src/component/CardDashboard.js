d672e9df56d6b151a5815f139939eacfb6b62299
diahfani <diahaufaarini5@gmail.com>
2021-12-02 00:24:17 +0700

    bismillah beneran fix

--- src/component/CardDashboard.js
+++ src/component/CardDashboard.js
@@ -2,13 +2,13 @@ import React from 'react'
 import { Table } from 'react-bootstrap'
 import '../style/CardDashboard.css'
 import { TOTAL_LAYANAN, TOTAL_THERAPIST } from '../store/queries'
-import { useQuery } from '@apollo/client'
+import { useQuery, useSubscription } from '@apollo/client'
 import Loading from './loading'
 // import {GET_THERAPIST, GET_LAYANAN} from '../store/queries'
 // import ListTabelTherapist from './ListTabelTherapist'
 
 function CardTotalTherapist() {
-    const { data: totalTherapist, loading: loadingTotalTherapist } = useQuery(TOTAL_THERAPIST)
+    const { data: totalTherapist, loading: loadingTotalTherapist } = useSubscription(TOTAL_THERAPIST)
     console.log(totalTherapist?.mini_project_therapist_aggregate?.aggregate.count)
     return (
         <div style={{ textAlign: 'center', width: '270px', height: '100px', background: '#EAF6F5', border: '1px solid #EAF6F5', boxSizing: 'border-box', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: '35px' }}>
@@ -23,8 +23,7 @@ function CardTotalTherapist() {
 }
 
 function CardTotalLayanan() {
-    const { data: totalLayanan, loading: loadingTotallayanan } = useQuery(TOTAL_LAYANAN)
-
+    const { data: totalLayanan, loading: loadingTotallayanan } = useSubscription(TOTAL_LAYANAN)
     return (
         <div style={{ textAlign: 'center', width: '270px', height: '100px', background: '#EAF6F5', border: '1px solid #EAF6F5', boxSizing: 'border-box', boxShadow: '0px 4px 4px rgba(0, 0, 0, 0.25)', borderRadius: '35px' }}>
             <h2 style={{ fontSize: '20px', paddingTop: '10px', color: '#0E483F' }}>Jumlah Layanan</h2>
